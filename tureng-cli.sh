52631eb48c2e64e516e4313f7d75abd2859d2eee
mebaysan <menesbaysan@gmail.com>
2022-10-08 09:37:33 +0300

    tureng-cli.sh improved to use all commands as bash script

--- tureng-cli.sh
+++ tureng-cli.sh
@@ -1,5 +1,14 @@
 #! /bin/bash
 
-SEARCH_TERM=$1
+N_RESULT=15
 
-python3 -m tureng_cli translate -w "$SEARCH_TERM" -n 15
+if [[ -n $3 ]]; then
+	N_RESULT=$3
+fi
+
+
+if [[ $1 != "translate" && $1 != "sentence" && $1 != "synonym" ]]; then
+	python3 -m tureng_cli translate  -w "$1" -n $N_RESULT
+else
+	python3 -m tureng_cli $1 -w "$2" -n $N_RESULT
+fi
