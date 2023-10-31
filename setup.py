33e2eca5eadd266ea257511ad4cfc1ec305bcedd
mebaysan <menesbaysan@gmail.com>
2023-10-31 11:26:45 +0300

    now package can be run with `tureng` command

--- setup.py
+++ setup.py
@@ -8,4 +8,5 @@ setup(
     packages=find_packages(include=["tureng_cli", "tureng_cli.*"]),
     install_requires=["beautifulsoup4", "click", "requests"],
     python_requires=">=3.8",
+    entry_points={"console_scripts": ["tureng=tureng_cli.__main__:main"]},
 )
