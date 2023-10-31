8759c52144d8c409ef3161694571c25694c39740
mebaysan <menesbaysan@gmail.com>
2023-10-31 15:58:01 +0300

    fix setup.py

--- setup.py
+++ setup.py
@@ -1,11 +1,20 @@
+import pathlib
 from setuptools import setup, find_packages
+
+# The directory containing this file
+HERE = pathlib.Path(__file__).parent
+
+README = (HERE / "README.md").read_text()
 
 setup(
     author="@mebaysan",
     author_email="mail@mebaysan.com",
     description="https://tureng.com/en/turkish-english CLI for translation",
     name="tureng-cli",
+    URL="https://github.com/mebaysan/Tureng-Translation-CLI",
     version="1.0.0",
+    long_descp_content="text/markdown",
+    long_descp=README,
     packages=find_packages(include=["tureng_cli", "tureng_cli.*"]),
     install_requires=["beautifulsoup4", "click", "requests"],
     python_requires=">=3.8",
